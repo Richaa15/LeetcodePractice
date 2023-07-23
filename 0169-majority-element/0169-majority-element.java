160c154c3c86ad12a07cc73759c0b66c3c35166d
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-07-23 12:00:08 -0700

    Time: 2 ms (64.67%), Space: 48.5 MB (58.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int ans = 0;
-        int count = 0;
-        
-        for (int i = 0; i <= n/2; i++) {
-            count = 0; // Reset the count for the current element
-            for (int j = i; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if (count > n / 2) {
-                ans = nums[i];
-                break; // No need to continue checking once we find the majority element
-            }
-        }
-        return ans;
+        Arrays.sort(nums);
+        return nums[n/2];
     }
 }
 
