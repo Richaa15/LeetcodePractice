dde44c7edfd34f341ffccd2839a2315073fdf42a
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-07-23 11:31:21 -0700

    Time: 1485 ms (5.03%), Space: 48.4 MB (62.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,9 +4,9 @@ class Solution {
         int ans = 0;
         int count = 0;
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n/2; i++) {
             count = 0; // Reset the count for the current element
-            for (int j = 0; j < n; j++) {
+            for (int j = i; j < n; j++) {
                 if (nums[i] == nums[j]) {
                     count++;
                 }
