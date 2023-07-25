0d487d38bbbe9e0eece0f624270c037c2f5eaffd
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-07-25 12:14:13 +0530

    Time: 0 ms (100.00%), Space: 55.2 MB (67.26%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,13 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int l = 0, r = arr.length - 1, mid;
-        while (l < r) {
-            mid = (l + r) / 2;
-            if (arr[mid] < arr[mid + 1])
-                l = mid + 1;
+        //s = start, e = end
+        int s = 0, e = arr.length - 1, mid;
+        while (s < e) {
+            mid = (s + e) / 2;
+            if (arr[mid] < arr[mid + 1]) //increasing part of array
+                s = mid + 1; //Peak element is in the right part
             else
-                r = mid;
+                e = mid; //Decreasing part of array
+            //So ned to checck in left part
+            //can't ignore mid element in this part
         }
-        return l;
+        return s;
+        //l and r are always trying to find maximum elements in above 2 checks.
+        //When they will point to the same element
+        //That will be the peak elemnt
     }
 }
