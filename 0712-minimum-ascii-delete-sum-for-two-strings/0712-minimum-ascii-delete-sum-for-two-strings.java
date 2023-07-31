340f8c1b6e761b49d9aac4fbc8e561c013617ace
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-07-31 21:41:54 +0530

    Time: 19 ms (93.00%), Space: 44.2 MB (63.51%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,47 +1,33 @@
 class Solution {
-
-    // Hash Map to store the result of each sub-problem.
-    Map<Pair<Integer, Integer>, Integer> savedResult = new HashMap<>();
-        
     public int minimumDeleteSum(String s1, String s2) {
         
-        // Return minimum cost to make s1[0...i] and s2[0...j] equal
-        return computeCost(s1, s2, s1.length()-1, s2.length()-1);
-    }
+        // Prepare the two-dimensional array
+        int m = s1.length(), n = s2.length();
+        int[][] computeCost = new int[m + 1][n + 1];
 
-    private int computeCost(String s1, String s2, int i, int j) {
-
-        // If both strings are empty, then no deletion is required
-        if (i < 0 && j < 0) {
-            return 0;
+        // Fill in the base case values
+        for (int i = 1; i <= m; i++) {
+            computeCost[i][0] = computeCost[i-1][0] + s1.charAt(i-1);
         }
-
-        // If already computed, then return the result from the hash map
-        Pair<Integer, Integer> key = new Pair<>(i, j);
-        if (savedResult.containsKey(key)) {
-            return savedResult.get(key);
+        for (int j = 1; j <= n; j++) {
+            computeCost[0][j] = computeCost[0][j-1] + s2.charAt(j-1);
         }
         
-        // If any one string is empty, delete all characters of the other string
-        if (i < 0) {
-            savedResult.put(key, s2.charAt(j) + computeCost(s1, s2, i, j-1));
-            return savedResult.get(key);
-        }
-        if (j < 0) {
-            savedResult.put(key, s1.charAt(i) + computeCost(s1, s2, i-1, j));
-            return savedResult.get(key);
+        // Fill the remaining cells using the Bellman Equation
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    computeCost[i][j] = computeCost[i-1][j-1];
+                } else {
+                    computeCost[i][j] = Math.min(
+                        s1.charAt(i-1) + computeCost[i-1][j],
+                        s2.charAt(j-1) + computeCost[i][j-1]
+                    );
+                }
+            }
         }
         
-        // Call sub-problem depending on s1[i] and s2[j]
-        // Save the computed result.
-        if (s1.charAt(i) == s2.charAt(j)) {
-            savedResult.put(key, computeCost(s1, s2, i-1, j-1));
-        } else {
-            savedResult.put(key, Math.min(
-                s1.charAt(i) + computeCost(s1, s2, i-1, j),
-                s2.charAt(j) + computeCost(s1, s2, i, j-1)
-            ));
-        }
-        return savedResult.get(key);
+        // Return the answer for entire input strings
+        return computeCost[m][n];
     }
 }
