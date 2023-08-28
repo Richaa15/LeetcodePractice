0e66f4dcd242d0224ca18facedbe27f0eda239eb
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-08-28 21:14:38 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (48.28%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -2,37 +2,42 @@ class MyStack {
     private Queue<Integer> q = new LinkedList<>();
 
     MyStack() {
-        
+        // Constructor for the MyStack class. This initializes a private member variable 'q' as a new LinkedList.
     }
 
     void push(int x) {
-        q.add(x);
-        int v = q.size() - 1;
-        int i = 0;
+        // This method is used to push an element onto the stack.
+
+        q.add(x); // Add the new element 'x' to the queue 'q'.
+
+        int v = q.size() - 1; // Get the current size of the queue 'q' and subtract 1, storing it in 'v'.
+        int i = 0; // Initialize a variable 'i' to 0.
+
         while (i < v) {
-            q.add(q.poll());
-            i++;
+            // Enter a loop that will run 'v' times (size - 1 times).
+
+            q.add(q.poll()); // Remove the front element of the queue using poll() and add it to the back.
+            i++; // Increment the counter 'i'.
         }
+        // This loop effectively rearranges the queue's elements so that the newly added element is at the front,
+        // simulating the behavior of a stack.
     }
 
     int pop() {
-        return q.poll();
+        // This method is used to pop (remove and return) the top element from the stack (queue in this case).
+
+        return q.poll(); // Remove and return the front element of the queue.
     }
 
     int top() {
-        return q.peek();
+        // This method is used to get the value of the top element in the stack (queue in this case).
+
+        return q.peek(); // Return the front element of the queue without removing it.
     }
 
     boolean empty() {
-        return q.isEmpty();
+        // This method checks if the stack (queue) is empty.
+
+        return q.isEmpty(); // Return true if the queue is empty, false otherwise.
     }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
