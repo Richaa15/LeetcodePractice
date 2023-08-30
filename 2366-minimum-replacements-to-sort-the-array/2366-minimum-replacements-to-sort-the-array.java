24a4a5e6d80ec2d3b78ee8eec904c1fd2aa9b379
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-08-30 23:10:56 +0530

    Time: 4 ms (86.46%), Space: 54.8 MB (47.92%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
@@ -1,25 +1,22 @@
 class Solution {
     public long minimumReplacement(int[] nums) {
-        long answer = 0;
         int n = nums.length;
+        int last = nums[n - 1];  // Initialize 'last' with the last element
+        long ans = 0;  // Initialize the total operations count
 
-        // Start from the second last element, as the last one is always sorted.
-        for (int i = n - 2; i >= 0; i--) {
-            // No need to break if they are already in order.
-            if (nums[i] <= nums[i + 1]) {
-                continue;
+        // Traverse the array in reverse order
+        for (int i = n - 2; i >= 0; --i) {
+            if (nums[i] > last) {  // If the current element needs replacement
+                int t = nums[i] / last;  // Calculate how many times the element needs to be divided
+                if (nums[i] % last != 0) {
+                    t++;  // If there's a remainder, increment 't'
+                }
+                last = nums[i] / t;  // Update 'last' for the next comparison
+                ans += t - 1;  // Add (t - 1) to 'ans' for the number of operations
+            } else {
+                last = nums[i];  // Update 'last' without replacement
             }
-
-            // Count how many elements are made from breaking nums[i].
-            long numElements = (long)(nums[i] + nums[i + 1] - 1) / (long)nums[i + 1];
-
-            // It requires numElements - 1 replacement operations.
-            answer += numElements - 1;
-
-            // Maximize nums[i] after replacement.
-            nums[i] = nums[i] / (int)numElements;
         }
-
-        return answer;
+        return ans;  // Return the total number of operations
     }
 }
