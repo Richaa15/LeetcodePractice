08dabeec36ca60127707cb924c669d4aa4a39cd7
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-09-26 01:07:11 +0530

    Time: 10 ms (15.47%), Space: 41.1 MB (13.49%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,8 +1,25 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char c = 0;
-        for(char cs : s.toCharArray()) c ^= cs;
-        for(char ct : t.toCharArray()) c ^= ct;
-        return c;
+        Map<Character, Integer> charCount = new HashMap<>();
+        
+        // Count the frequency of characters in string t
+        for (char c : t.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        
+        // Subtract the frequency of characters in string s
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.get(c) - 1);
+        }
+        
+        // Find the character with a positive count (added character)
+        for (char c : t.toCharArray()) {
+            if (charCount.get(c) > 0) {
+                return c;
+            }
+        }
+        
+        // Return a dummy value (should not reach here)
+        return '\0';
     }
 }
