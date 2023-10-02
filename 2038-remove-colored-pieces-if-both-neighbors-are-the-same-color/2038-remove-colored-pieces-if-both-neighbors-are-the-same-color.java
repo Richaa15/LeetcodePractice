b04f1e7209ec03b39a1bb2ab4a169b418ab81374
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-10-03 00:44:41 +0530

    Time: 16 ms (52.37%), Space: 44.1 MB (68.76%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        int aliceScore = 0;
-        int bobScore = 0;
-
-        // Iterate through the colors, excluding the edge pieces
+        int alice = 0;
+        int bob = 0;
+        
         for (int i = 1; i < colors.length() - 1; i++) {
-            char currentColor = colors.charAt(i);
-            char prevColor = colors.charAt(i - 1);
-            char nextColor = colors.charAt(i + 1);
-
-            // Check if Alice can make a move here
-            if (currentColor == 'A' && prevColor == 'A' && nextColor == 'A')
-                aliceScore++; // Alice can remove 'A'
-
-            // Check if Bob can make a move here
-            else if (currentColor == 'B' && prevColor == 'B' && nextColor == 'B')
-                bobScore++; // Bob can remove 'B'
+            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
+                if (colors.charAt(i) == 'A') {
+                    alice++;
+                } else {
+                    bob++;
+                }
+            }
         }
-
-        // Determine the winner based on the scores
-        return aliceScore > bobScore;
+        
+        return alice - bob >= 1;
     }
 }
