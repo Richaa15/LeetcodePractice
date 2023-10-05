5b5ed48ad89ba314d29739a16f8819d3d197321d
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-10-05 19:39:59 +0530

    Time: 2 ms (98.52%), Space: 46.6 MB (55.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-       int n = arr.length; //size of the array
+        int n = arr.length; //size of the array
         List<Integer> result = new ArrayList<>(); // list of answers
 
-        //declaring a map:
-        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-
-        // least occurrence of the majority element:
-        int minCount = (int)(n / 3) + 1;
-
-        //storing the elements with their occurrence:
         for (int i = 0; i < n; i++) {
-            int value = frequencyMap.getOrDefault(arr[i], 0);
-            frequencyMap.put(arr[i], value + 1);
+            //selected element is arr[i]:
+            // Checking if arr[i] is not already
+            // a part of the answer:
+            if (result.size() == 0 || result.get(0) != arr[i]) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    // counting the frequency of arr[i]
+                    if (arr[j] == arr[i]) {
+                        count++;
+                    }
+                }
 
-            //checking if arr[i] is
-            // the majority element:
-            if (frequencyMap.get(arr[i]) == minCount) {
-                result.add(arr[i]);
+                // check if frequency is greater than n/3:
+                if (count > (n / 3))
+                    result.add(arr[i]);
             }
+
             if (result.size() == 2) break;
         }
 
