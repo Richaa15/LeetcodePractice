a8d981912a247e817c0194b92cabcb4579d08aac
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-10-14 22:15:37 +0530

    Time: 10 ms (82.35%), Space: 43.6 MB (83.33%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,20 +1,22 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length;
-        int[][] dp = new int[n + 1][n + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            dp[n][i] = (int) 1e9;
-        }
-        
+        int[] dp = new int[n + 1];
+        int[] prevDp = new int[n + 1];
+        Arrays.fill(prevDp, (int) 1e9);
+        prevDp[0] = 0;
+
         for (int i = n - 1; i >= 0; i--) {
+            dp = new int[n + 1];
             for (int remain = 1; remain <= n; remain++) {
-                int paint = cost[i] + dp[i + 1][Math.max(0, remain - 1 - time[i])];
-                int dontPaint = dp[i + 1][remain];
-                dp[i][remain] = Math.min(paint, dontPaint);
+                int paint = cost[i] + prevDp[Math.max(0, remain - 1 - time[i])];
+                int dontPaint = prevDp[remain];
+                dp[remain] = Math.min(paint, dontPaint);
             }
+            
+            prevDp = dp;
         }
         
-        return dp[0][n];
+        return dp[n];
     }
 }
