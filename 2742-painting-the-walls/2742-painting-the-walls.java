9675ed1d8e31fc0745f5539797eb293a84ef4a99
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-10-14 22:15:12 +0530

    Time: 11 ms (77.45%), Space: 43.9 MB (71.08%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,29 +1,20 @@
 class Solution {
-    int[][] memo;
-    int n;
-    
     public int paintWalls(int[] cost, int[] time) {
-        n = cost.length;
-        memo = new int[n][n + 1];
-        return dp(0, n, cost, time);
-    }
-    
-    public int dp(int i, int remain, int[] cost, int[] time) {
-        if (remain <= 0) {
-            return 0;
+        int n = cost.length;
+        int[][] dp = new int[n + 1][n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            dp[n][i] = (int) 1e9;
         }
         
-        if (i == n) {
-            return (int) 1e9;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int remain = 1; remain <= n; remain++) {
+                int paint = cost[i] + dp[i + 1][Math.max(0, remain - 1 - time[i])];
+                int dontPaint = dp[i + 1][remain];
+                dp[i][remain] = Math.min(paint, dontPaint);
+            }
         }
         
-        if (memo[i][remain] != 0) {
-            return memo[i][remain];
-        }
-        
-        int paint = cost[i] + dp(i + 1, remain - 1 - time[i], cost, time);
-        int dontPaint = dp(i + 1, remain, cost, time);
-        memo[i][remain] = Math.min(paint, dontPaint);
-        return memo[i][remain];
+        return dp[0][n];
     }
 }
