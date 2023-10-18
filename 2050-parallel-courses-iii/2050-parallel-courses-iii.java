8473576e2568a314eb053e4a5100d6daa31b11ee
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-10-18 22:47:09 +0530

    Time: 42 ms (20.89%), Space: 79.8 MB (5.78%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,45 +1,41 @@
 class Solution {
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    Map<Integer, Integer> memo = new HashMap<>();
+    
     public int minimumTime(int n, int[][] relations, int[] time) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.put(i, new ArrayList<>());
         }
         
-        int[] indegree = new int[n];
         for (int[] edge: relations) {
             int x = edge[0] - 1;
             int y = edge[1] - 1;
             graph.get(x).add(y);
-            indegree[y]++;
-        }
-        
-        Queue<Integer> queue = new LinkedList<>();
-        int[] maxTime = new int[n];
-        
-        for (int node = 0; node < n; node++) {
-            if (indegree[node] == 0) {
-                queue.add(node);
-                maxTime[node] = time[node];
-            }
-        }
-        
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-            for (int neighbor: graph.get(node)) {
-                maxTime[neighbor] = Math.max(maxTime[neighbor], maxTime[node] + time[neighbor]);
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    queue.add(neighbor);
-                }
-            }
         }
         
         int ans = 0;
         for (int node = 0; node < n; node++) {
-            ans = Math.max(ans, maxTime[node]);
+            ans = Math.max(ans, dfs(node, time));
         }
 
         return ans;
     }
     
+    public int dfs(int node, int[] time) {
+        if (memo.containsKey(node)) {
+            return memo.get(node);
+        }
+        
+        if (graph.get(node).size() == 0) {
+            return time[node];
+        }
+        
+        int ans = 0;
+        for (int neighbor: graph.get(node)) {
+            ans = Math.max(ans, dfs(neighbor, time));
+        }
+        
+        memo.put(node, time[node] + ans);
+        return time[node] + ans;
+    }
 }
