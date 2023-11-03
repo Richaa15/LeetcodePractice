3b25907022c95d5404e988c547d1fb788d006e11
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2023-11-03 23:21:04 +0530

    Time: 1540 ms (17.13%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,3 +1,3 @@
-SELECT user_id, CONCAT(UPPER(LEFT(name,1)), LOWER(RIGHT(name,LENGTH(name)-1))) AS name
-FROM Users
-ORDER BY user_id ASC;
+select user_id , CONCAT( UPPER( SUBSTR(name,1,1) ) , LOWER( SUBSTR(name,2) ) ) as name
+    from Users 
+    order by user_id;
