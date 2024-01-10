314d7490d451d46c12d4d07e2e0641a8f68071a5
Richa Rai <92245354+Richaa15@users.noreply.github.com>
2024-01-10 23:19:31 +0530

    Time: 8 ms (97.91%), Space: 78.1 MB (59.19%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,50 +14,32 @@
  * }
  */
 class Solution {
+    private int maxDistance = 0;
     public int amountOfTime(TreeNode root, int start) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();  
-        convert(root, 0, map);
-        Queue<Integer> queue = new LinkedList<>(); 
-        queue.add(start);
-        int minute = 0;
-        Set<Integer> visited = new HashSet<>();
-        visited.add(start);
-          
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            while (levelSize > 0) {
-                int current = queue.poll();
-                for (int num : map.get(current)) {
-                    if (!visited.contains(num)) {
-                        visited.add(num);
-                        queue.add(num);
-                    }
-                }
-                levelSize--;
-            }
-            minute++;
-        }
-        return minute - 1;
+        traverse(root, start);
+        return maxDistance;
     }
- 
-    public void convert(TreeNode current, int parent, Map<Integer, Set<Integer>> map){
-        if (current == null) {
-            return;
-        } 
-        if (!map.containsKey(current.val)) {
-            map.put(current.val, new HashSet<>());
-        } 
-        Set<Integer> adjacentList = map.get(current.val);
-        if (parent != 0) {
-            adjacentList.add(parent);
-        } 
-        if (current.left != null) {
-            adjacentList.add(current.left.val);
-        } 
-        if (current.right != null) {
-            adjacentList.add(current.right.val);
-        } 
-        convert(current.left, current.val, map);
-        convert(current.right, current.val, map);
-    }      
+
+    public int traverse(TreeNode root, int start) {
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+
+        int leftDepth = traverse(root.left, start);
+        int rightDepth = traverse(root.right, start);
+
+        if (root.val == start) {
+            maxDistance = Math.max(leftDepth, rightDepth);
+            depth = -1;
+        } else if (leftDepth >= 0 && rightDepth >= 0) {
+            depth = Math.max(leftDepth, rightDepth) + 1;
+        } else {
+            int distance = Math.abs(leftDepth) + Math.abs(rightDepth);
+            maxDistance = Math.max(maxDistance, distance);
+            depth = Math.min(leftDepth, rightDepth) - 1;
+        }
+
+        return depth;
+    }
 }
